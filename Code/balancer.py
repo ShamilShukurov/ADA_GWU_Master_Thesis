69a95b60bd1713e06e81b431d50c6d70cef21f1f
ShamilShukurov <shamill.shukurov@gmail.com>
2024-03-21 22:56:57 +0400

    random_under_sampling implemented

--- Code/balancer.py
+++ Code/balancer.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 from abc import ABC, abstractmethod
 from imblearn.over_sampling import RandomOverSampler
+from imblearn.under_sampling import RandomUnderSampler
 
 
 class Balancer(ABC):
@@ -35,6 +36,26 @@ class RandomOverSamplerBalancer(Balancer):
         X_resampled, y_resampled = self.ros.fit_resample(x_train, y_train)
         return pd.concat([pd.DataFrame(X_resampled, columns=x_train.columns), 
                           pd.Series(y_resampled)],axis=1)
+
+    @property
+    def name(self) -> str:
+        """Returns the name of the algorithm."""
+        return self._name
+    
+
+class RandomUnderSamplerBalancer(Balancer):
+    """Random Under Sampler implementation of the Balancer abstract class."""
+
+    def __init__(self):
+        self.rus = RandomUnderSampler(random_state=42)
+        self._name = "RandomUnderSample"
+
+    def balance_data(self, x_train: pd.DataFrame, y_train: pd.Series) -> pd.DataFrame:
+        X_resampled, y_resampled = self.rus.fit_resample(x_train, y_train)
+        # Concatenating the resampled features and target into a single DataFrame
+        balanced_data = pd.concat([pd.DataFrame(X_resampled, columns=x_train.columns), pd.Series(y_resampled, name=y_train.name)], axis=1)
+        
+        return balanced_data
 
     @property
     def name(self) -> str:
