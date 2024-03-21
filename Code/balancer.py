847fd35277d8735597c3c2a38dbe17577e6287cf
ShamilShukurov <shamill.shukurov@gmail.com>
2024-03-21 23:01:19 +0400

    SMOTE implemented

--- Code/balancer.py
+++ Code/balancer.py
@@ -3,20 +3,23 @@ import numpy as np
 from abc import ABC, abstractmethod
 from imblearn.over_sampling import RandomOverSampler
 from imblearn.under_sampling import RandomUnderSampler
-
+from imblearn.over_sampling import SMOTE
 
 class Balancer(ABC):
   """Wrapper class for Data Balancing algorithms."""
 
   @abstractmethod
   def balance_data(self, x_train:pd.DataFrame, y_train:np.array) -> pd.DataFrame:
-    """
+      """
         Abstract method to balance the input dataset.
-        Parameters:
-        - x_train (pd.DataFrame): Input DataFrame containing training data.
-        Returns:
-        - pd.DataFrame: Balanced DataFrame after applying the balancing algorithm.
-    """
+      
+      Parameters:
+      - x_train (pd.DataFrame): Input DataFrame containing features.
+      - y_train (pd.Series): Input Series containing the target variable.
+      
+      Returns:
+      - pd.DataFrame: Balanced DataFrame after applying SMOTE.
+      """
   @property
   @abstractmethod
   def name(self) -> str:
@@ -52,6 +55,26 @@ class RandomUnderSamplerBalancer(Balancer):
 
     def balance_data(self, x_train: pd.DataFrame, y_train: pd.Series) -> pd.DataFrame:
         X_resampled, y_resampled = self.rus.fit_resample(x_train, y_train)
+        # Concatenating the resampled features and target into a single DataFrame
+        balanced_data = pd.concat([pd.DataFrame(X_resampled, columns=x_train.columns), pd.Series(y_resampled, name=y_train.name)], axis=1)
+        
+        return balanced_data
+
+    @property
+    def name(self) -> str:
+        """Returns the name of the algorithm."""
+        return self._name
+    
+
+class SMOTEBalancer(Balancer):
+    """SMOTE implementation of the Balancer abstract class."""
+
+    def __init__(self, sampling_strategy='auto', random_state=42):
+        self.smote = SMOTE(sampling_strategy=sampling_strategy, random_state=random_state)
+        self._name = "SMOTE"
+
+    def balance_data(self, x_train: pd.DataFrame, y_train: pd.Series) -> pd.DataFrame:
+        X_resampled, y_resampled = self.smote.fit_resample(x_train, y_train)
         # Concatenating the resampled features and target into a single DataFrame
         balanced_data = pd.concat([pd.DataFrame(X_resampled, columns=x_train.columns), pd.Series(y_resampled, name=y_train.name)], axis=1)
         
