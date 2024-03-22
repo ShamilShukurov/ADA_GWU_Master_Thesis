f32e7550371a06e36963d95ccd8ab70e7659c2e4
ShamilShukurov <shamill.shukurov@gmail.com>
2024-03-22 11:53:59 +0400

    plot_roc_curve moved to abstract class

--- Code/base_learning.py
+++ Code/base_learning.py
@@ -1,6 +1,8 @@
 import numpy as np
 import pandas as pd
 from abc import ABC, abstractmethod
+import matplotlib.pyplot as plt
+from sklearn.metrics import f1_score, precision_score, accuracy_score, recall_score, roc_auc_score, roc_curve, auc
 
 class BaseLearningAlgorithm(ABC):
   """Base class for a Supervised Learning Algorithm."""
@@ -26,7 +28,21 @@ class BaseLearningAlgorithm(ABC):
     """Trains the model and return model evaluation report based on train and test data.
         --save_model (bool): Flag to indicate whether to save the trained model.
     """
+  def plot_roc_curve(self, y_true: np.array, probabilities: np.array, dataset_label: str) -> None:
+      """Plot the ROC curve for a given dataset."""
+      fpr, tpr, _ = roc_curve(y_true, probabilities)
+      roc_auc = auc(fpr, tpr)
 
+      plt.figure()
+      plt.plot(fpr, tpr, color='darkorange', lw=2, label=f'ROC curve ({dataset_label} - area = %0.2f)' % roc_auc)
+      plt.plot([0, 1], [0, 1], color='navy', lw=2, linestyle='--')
+      plt.xlim([0.0, 1.0])
+      plt.ylim([0.0, 1.05])
+      plt.xlabel('False Positive Rate')
+      plt.ylabel('True Positive Rate')
+      plt.title(f'Receiver Operating Characteristic - {dataset_label}')
+      plt.legend(loc="lower right")
+      plt.show()
   @property
   @abstractmethod
   def name(self) -> str:
